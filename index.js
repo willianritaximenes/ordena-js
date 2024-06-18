dc3fbf4e1f33a0d9689db3d0af764f59a9957e23
willianritaximenes <willianritaximenes@gmail.com>
2024-06-18 18:27:01 -0300

    init: ordenacao

---
+++ index.js
@@ -0,0 +1,17 @@
+let numeros = [4, 3, 1, 5, 7, 2, -47, 2001]
+for (let i = 0; i < numeros.length; i++) {
+
+    for (let j = i + 1; j < numeros.length; j++){
+     
+
+        if(numeros[i] > numeros[j]) {
+            let aux = numeros[i]
+            numeros[i] = numeros[j]
+            numeros[j] = aux
+        }else if(numeros[i]==numeros[j]){
+            numeros.splice(j,1)
+        }
+    }
+}
+
+console.log(numeros)
